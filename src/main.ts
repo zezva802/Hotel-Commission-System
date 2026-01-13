a6ae50a23330566e81f5af63e0a986a64053fc86
zezva <kobaidze.zezva@kiu.edu.ge>
2026-01-13 19:52:24 +0400

    feat: set up NestJS application

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true
+        })
+    );
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+
+    console.log(`application running http://localhost:${port}`)
+}
+
+bootstrap();
